c04ed26a249adcc0640c22c84d3ab62be8f52cf7
Javier Fraire <javierfraire92@gmail.com>
2015-11-29 01:56:46 -0300

    added light intensity

--- core/src/com/tvja/render/Light.java
+++ core/src/com/tvja/render/Light.java
@@ -9,12 +9,14 @@ public class Light extends ViewWorldObject {
     static float nearZ = 0.01f;
     static float farZ = 100;
     protected Vector3 color;
+    private float intensity;
     private Float angle;
 
-    private Light(Vector3 position, Vector3 orientation, Vector3 color, Float angle) {
+    private Light(Vector3 position, Vector3 orientation, Vector3 color, Float angle, float intensity) {
         super(nearZ, farZ, position, orientation);
 
         this.color = color;
+        color.scl(intensity);
 
         if (angle != null) {
             if (Math.abs(angle) > Math.PI / 2) {
@@ -26,22 +28,22 @@ public class Light extends ViewWorldObject {
 
     //TODO: Change projection matrix parameters for all 3 constructors
     
-    public static Light newDirectional(Vector3 orientation, Vector3 color) {
-        Light l = new Light(new Vector3(), orientation, color, null);
+    public static Light newDirectional(Vector3 orientation, Vector3 color, float intensity) {
+        Light l = new Light(new Vector3(), orientation, color, null, intensity);
         Vector3 dir = l.getDirection().nor();
         l.setPosition(dir.scl(10));
         l.projection = MathUtils.genOrthogonalProjection(nearZ, farZ, 20, 20);
         return l;
     }
 
-    public static Light newPoint(Vector3 position, Vector3 color) {
-        Light l = new Light(position, new Vector3(), color, null);
+    public static Light newPoint(Vector3 position, Vector3 color, float intensity) {
+        Light l = new Light(position, new Vector3(), color, null, intensity);
         l.projection = MathUtils.genPerspectiveProjection(nearZ, farZ, 60, 60);
         return l;
     }
 
-    public static Light newSpot(Vector3 position, Vector3 orientation, Vector3 color, Float angle) {
-        Light l = new Light(position, orientation, color, angle);
+    public static Light newSpot(Vector3 position, Vector3 orientation, Vector3 color, Float angle, float intensity) {
+        Light l = new Light(position, orientation, color, angle, intensity);
         l.projection = MathUtils.genPerspectiveProjection(nearZ, farZ, 60, 60);
         return l;
     }
